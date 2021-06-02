1740e79ade1dd8ee2e5e984f651fe4e88977fabb
ShawonAshraf <shawon13@live.com>
2021-06-02 17:10:32 +0200

    ex4 parser done

--- assignment_4/top_down_augmented_solution.py
+++ assignment_4/top_down_augmented_solution.py
@@ -3,16 +3,17 @@
 # course "Parsing"
 
 # enables input history in interactive mode
-import readline
+# import readline
 
+import copy
 from nltk import CFG, Tree
-
 
 # Boolean variable for switching tracing info on and off
 trace = True  # set this to False if you don't want to see intermediate steps
 
 # Boolean variable for running parser interactively on user input or on pre-specified input
-interactive = True  # False
+# interactive = True  # False
+interactive = False
 
 # internal format of cfg production rules with reversed right-hand sides (!)
 # grammar = {'S': [['VP', 'NP']],
@@ -40,6 +41,7 @@ Op   -> '+' | '-' | '*' | '/'
 Sign -> '-'
 """
 
+# load this for exercise 4.1
 grammar_string3 = """
 S -> NP VP
 NP -> DET N | DET N PP | 'I'
@@ -82,6 +84,99 @@ def build_tree(deriv):
         return Tree(nt, subtrees), deriv_rest
     else:
         return [], []
+
+
+# parse with agendas for backtracking
+def parse_backtrack(G, tokens):
+    # initialize data structures:
+    stack = ['S']
+    inbuffer = tokens
+    seq = []  # keep track of expansions for backtracking
+    agenda = []
+    solutions = []
+
+    # main loop:
+    while True:
+        if trace: print('           {:<40}{:>40}'.format(str(stack), str(inbuffer)))
+
+        # expand
+        if stack != [] and inbuffer != [] and stack[-1] in G:
+            stack_top = stack[-1]
+            if [inbuffer[0]] in G[stack_top]:
+                if trace:
+                    print(" >expand:   ", stack_top, "    -R->    ", G[stack_top][0])
+
+                # which inbuffer element in which prod
+                inbuffer_el_idx = G[stack_top].index([inbuffer[0]])
+                # print(inbuffer_el_idx)
+                rhs = G[stack_top][inbuffer_el_idx]
+                # print(rhs)
+                seq.append((stack_top, len(rhs)))  # add to tracker
+                del stack[-1]  # pop
+                stack += rhs
+            else:
+                for production in G[stack_top]:
+                    new_seq = copy.deepcopy(seq)
+                    new_stack = copy.deepcopy(stack)
+                    new_inbuffer = copy.deepcopy(inbuffer)
+                    new_seq.append((new_stack[-1], len(production)))
+
+                    del new_stack[-1]  # pop
+
+                    new_stack += production
+                    latest_accessed_prod = production
+
+                    agenda.append((new_stack, new_inbuffer, new_seq))
+
+                stack = agenda[-1][0]
+                inbuffer = agenda[-1][1]
+                seq = agenda[-1][2]
+
+                del agenda[-1] # eliminate since already processed
+
+                if trace:
+                    print(" >expand:   ", stack_top, "    -R->    ", latest_accessed_prod)
+
+
+        # match
+        elif stack != [] and inbuffer != [] and stack[-1] == inbuffer[0]:
+            if trace: print(" >match:   ", stack[-1], "    -R->    ", inbuffer[0])
+            seq.append((stack[-1], 0))
+            del stack[-1]
+            del inbuffer[0]
+
+
+        # termination
+        elif stack == inbuffer == []:
+            if trace: print('           {:<40}{:>40}'.format(str(stack), str(inbuffer)))
+            solutions.append(seq)
+            print("found one solution!\n")
+            if agenda != []:
+                print("searching for more solutions/derivations...\n")
+                stack = agenda[-1][0]
+                inbuffer = agenda[-1][1]
+                seq = agenda[-1][2]
+                del agenda[-1]
+            else:
+                if solutions != []:
+                    print("failure!\n\n")
+                else:
+                    print("success!\n\n")
+                return solutions
+        else:
+            if trace: print(" >stuck!")
+            if agenda != []:
+                print("parsing for additional solutions\n")
+                stack = agenda[-1][0]
+                inbuffer = agenda[-1][1]
+                seq = agenda[-1][2]
+                del agenda[-1]
+            else:
+                if solutions == []:
+                    print("failure!\n\n")
+                else:
+                    print("success!\n\n")
+                return solutions
 
 
 # main procedure:
@@ -158,7 +253,7 @@ def parse(grammar, tokens):
     print()
 
 
-def demo(grammar):
+def demo(grammar, test_sentences=[]):
     # show internal representation of grammar
     if trace:
         print("Internal grammar representation:\n", grammar)
@@ -177,13 +272,20 @@ def demo(grammar):
                 # call actual parsing procedure:
                 parse(grammar, tokens)
     else:
-        tokens = "the elephant sneezed".split()
-        parse(grammar, tokens)
-        tokens = "my mouse giggled".split()
-        parse(grammar, tokens)
+        for test_sentence in test_sentences:
+            tokens = test_sentence.split(" ")
+            result = parse_backtrack(grammar, tokens)
+            build_tree(result)
 
 
 if __name__ == "__main__":
     # Convert grammar into our internal dict format
-    grammar = load_grammar(grammar_string)
-    demo(grammar)
+    grammar = load_grammar(grammar_string3)
+
+    test_sentences = [
+        "I shot an elephant in my pajamas",
+        # "the elephant sneezed",
+        # "my mouse giggled"
+    ]
+
+    demo(grammar, test_sentences)
